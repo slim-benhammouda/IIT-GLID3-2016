3409cda9bbca478030a3c0ca1822b02c7b4ea418
Slim BENHAMMOUDA <slim.benhammouda@gmail.com>
2015-10-07 16:22:54 +0100

    implement list view with custom adapter

--- app/src/main/java/com/iit/glid/glidapplication/MainActivity.java
+++ app/src/main/java/com/iit/glid/glidapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.iit.glid.glidapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import com.iit.glid.glidapplication.fragment.FragmentWithList;
 import com.iit.glid.glidapplication.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +25,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_layout, FragmentWithList.newInstance())
+                        .commit();
+                break;
+
+        }
+
+
         return true;
     }
 }
